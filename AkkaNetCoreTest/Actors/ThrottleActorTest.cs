a6ab1f53d8836ec06dc969b78517cde1d8558723
psmon <psmon@live.co.kr>
2020-03-07 22:43:21 +0900

    doc update

--- AkkaNetCoreTest/Actors/ThrottleActorTest.cs
+++ AkkaNetCoreTest/Actors/ThrottleActorTest.cs
@@ -15,22 +15,22 @@ namespace AkkaNetCoreTest.Actors
         [SetUp]
         public void Setup()
         {
-            //스트림을 제공받는 최종 소비자
+            //스트림을 제공받는 최종 소비자 ( 물을 제공 받는 고객 )
             probe = this.CreateTestProbe();
         }
 
         [TestCase(15)]
         public void ThrottleActorAreOK(int cutoffSec)
         {
-            // 초당 5개 처리한정
+            // 초당 5개 처리한정 ( 더 처리하고 싶으면 이값을 늘린다.)
             int timeSec = 1;
             int elemntPerSec = 5;            
             var throttleActor = Sys.ActorOf(Props.Create(() => new ThrottleActor(timeSec)));
             var throttleWork = Sys.ActorOf(Props.Create(() => new ThrottleWork(elemntPerSec, timeSec)));
 
             // 밸브에게 작업자 지정 ( 밸브는 초당 스트림을 모아서 방출한다 )
-            throttleActor.Tell(new SetTarget(throttleWork));
             // 작업자는 방류된 스트림을 기본적으로 쌓아두고, 초당 지정된 개수만 처리한다.
+            throttleActor.Tell(new SetTarget(throttleWork));            
 
             // 소비자지정 : 소비자는 몇개가 초당 처리되던 상관없이, 완료된 작업만 제공받는다.
             throttleWork.Tell(new SetTarget(probe));
