e3013b5c908fedaf95a626a5c6f9b9b35eb75e69
psmon <psmon@live.co.kr>
2020-03-10 00:51:37 +0900

    벌크 인서트 배치 - 성능테스트추가

---
+++ AkkaNetCoreTest/Repositories/BulkInsertTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AkkaNetCore.Config;
+using AkkaNetCore.Models.Entity;
+using AkkaNetCore.Repositories;
+using Xunit;
+using Xunit.Abstractions;
+using Z.EntityFramework.Extensions;
+
+namespace AkkaNetCoreTest.Repositories
+{
+    public class BulkInsertTest : TestKitXunit
+    {
+        private AppSettings appSettings;
+
+        public BulkInsertTest(ITestOutputHelper output) : base(output)
+        {
+            Setup();
+        }
+
+        public void Setup()
+        {
+            appSettings = new AppSettings()
+            {
+                DBConnection = "server=localhost;port=33061;database=showa_search;user=root;password=root;"
+            };
+        }
+
+        [Theory]
+        [InlineData(10000, 100, 10)]
+        public void BulkSppedTest(int daatSize,int batchSize, int cutoff)
+        {
+            var bulkItems_reseverd = new List<MessageReseved>();
+            for(int i = 0; i < daatSize; i++)
+            {
+                var addData = new MessageReseved()
+                {
+                    Seq = i.ToString(),
+                    no = i,
+                    Message = "TestMessage" + i,
+                    updateTime = DateTime.Now
+                };
+                bulkItems_reseverd.Add(addData);
+            }
+
+            Within(TimeSpan.FromSeconds(cutoff), () => {
+                EntityFrameworkManager.ContextFactory = context => new BatchRepository(appSettings);
+                using (var context = new BatchRepository(appSettings))
+                {
+                    context.BulkInsertAsync(bulkItems_reseverd, options => {
+                        options.BatchSize = batchSize;
+                    }).Wait();
+                }
+            });
+        }
+    }
+}
