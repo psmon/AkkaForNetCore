0598cf970a670eae549be98acc248c1ca7a82375
psmon <psmon@live.co.kr>
2020-03-15 22:09:31 +0900

    AtLeastOnceDelivery-Model

---
+++ AkkaNetCore/Models/Message/MsgConfirmed.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AkkaNetCore.Models.Message
+{
+    // AtLeastOnceDelivery를 위한 메시지 설계
+    public class Msg
+    {
+        public Msg(long deliveryId, string message)
+        {
+            DeliveryId = deliveryId;
+            Message = message;
+        }
+
+        public long DeliveryId { get; }
+
+        public string Message { get; }
+    }
+
+    public class Confirm
+    {
+        public Confirm(long deliveryId)
+        {
+            DeliveryId = deliveryId;
+        }
+
+        public long DeliveryId { get; }
+    }
+
+    public interface IEvent
+    {
+
+    }
+
+    public class MsgSent : IEvent
+    {
+        public MsgSent(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+
+    public class MsgConfirmed : IEvent
+    {
+        public MsgConfirmed(long deliveryId)
+        {
+            DeliveryId = deliveryId;
+        }
+
+        public long DeliveryId { get; }
+    }
+}
